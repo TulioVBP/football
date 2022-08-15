d8672c8cbff301656e19f8b7042b8f60fa9f9033
Tulio Patriota <tuliopatriota96@gmail.com>
2022-08-15 17:19:04 +0200

    Changed data preprocessing

--- DataPreprocessing.py
+++ DataPreprocessing.py
@@ -11,11 +11,12 @@ class DataPreprocessing:
             # Step 1 - Update the database 
             self.__updateDatabase()
             # Step 2 - Read the database
-            self.__readData()
-            # Step 3 - Expand the data values
-            self.__expandData()
-            # Step 4 - Save the data
-            self.__saveData()
+        self.__readData()
+        # Step 3 - Expand the data values
+        self.__expandData()
+        # Step 4 - Save the data
+        self.__saveData()
+
 
     def __updateDatabase(self):
         self.ScraperResults.loop_scrape()
@@ -56,6 +57,10 @@ class DataPreprocessing:
             self.df_rosters = df_rosters
     
     def __expandData(self):
+        schema_shift = ['season_points','scored_goals_season','missed_goals_season','avg_ppda.att','avg_ppda.def',
+                                'avg_ppda_allowed.att','avg_ppda_allowed.def','avg_deep','avg_deep_allowed']
+        schema_away = ['away_points','scored_goals_season_a','missed_goals_season_a','avg_ppda.att_a','avg_ppda.def_a',
+                                'avg_ppda_allowed.att_a','avg_ppda_allowed.def_a','avg_deep_a','avg_deep_allowed_a']
         # Global matchId
         self.df_rosters['date'] = pd.to_datetime(self.df_rosters['date']).dt.date # Pre-processing to have date in the same format as 
         for matchId in self.df_rosters['matchId'].unique():
@@ -67,33 +72,62 @@ class DataPreprocessing:
         self.df.sort_values(by = 'date',inplace = True)
         for team_id in self.df['team_id'].unique():
             for season in seasons:
-                idx = (self.df['team_id'] == team_id) & (self.df['season'] == season)
-                grp = self.df[idx].groupby('team_id')
+                idx = self.df.loc[(self.df['team_id'] == team_id) & (self.df['season'] == season),:].index
+                if len(idx) == 0:
+                    continue
+                grp = self.df.loc[idx,:].groupby('team_id')
                 #sh_grp = grp.apply(lambda p: p.shift(fill_value=0).cumsum())
                 #print(sh_grp)
-                self.df.loc[idx,'home_points'] = self.df[idx].pts.cumsum()
-                self.df.loc[idx,'scored_goals_season_h'] = self.df[idx].scored.cumsum()
-                self.df.loc[idx,'missed_goals_season_h'] = self.df[idx].missed.cumsum()
-                self.df.loc[idx,'avg_ppda.att_h'] = self.df.loc[idx,'ppda.att'].cumsum()/(grp.cumcount()+1)
-                self.df.loc[idx,'avg_ppda.def_h'] = self.df.loc[idx,'ppda.def'].cumsum()/(grp.cumcount()+1)
-                self.df.loc[idx,'avg_ppda_allowed.att_h'] = self.df.loc[idx,'ppda_allowed.att'].cumsum()/(grp.cumcount()+1)
-                self.df.loc[idx,'avg_ppda_allowed.def_h'] = self.df.loc[idx,'ppda_allowed.def'].cumsum()/(grp.cumcount()+1)
-                self.df.loc[idx,'avg_deep_h']= self.df.loc[idx,'deep'].cumsum()/(grp.cumcount()+1)
-                self.df.loc[idx,'avg_deep_allowed_h']= self.df.loc[idx,'deep_allowed'].cumsum()/(grp.cumcount()+1)
-        # Mirroring for away teams
+                self.df.loc[idx,'season_points'] = self.df.loc[idx,"pts"].cumsum()
+                self.df.loc[idx,'scored_goals_season'] = self.df.loc[idx,"scored"].cumsum()
+                self.df.loc[idx,'missed_goals_season'] = self.df.loc[idx,"missed"].cumsum()
+                self.df.loc[idx,'avg_ppda.att'] = self.df.loc[idx,'ppda.att'].cumsum()/(grp.cumcount()+1)
+                self.df.loc[idx,'avg_ppda.def'] = self.df.loc[idx,'ppda.def'].cumsum()/(grp.cumcount()+1)
+                self.df.loc[idx,'avg_ppda_allowed.att'] = self.df.loc[idx,'ppda_allowed.att'].cumsum()/(grp.cumcount()+1)
+                self.df.loc[idx,'avg_ppda_allowed.def'] = self.df.loc[idx,'ppda_allowed.def'].cumsum()/(grp.cumcount()+1)
+                self.df.loc[idx,'avg_deep']= self.df.loc[idx,'deep'].cumsum()/(grp.cumcount()+1)
+                self.df.loc[idx,'avg_deep_allowed']= self.df.loc[idx,'deep_allowed'].cumsum()/(grp.cumcount()+1)
+                # Shift the results
+                idx_shift = idx[1:]
+                self.df.loc[idx_shift,schema_shift] =  self.df.loc[idx[0:-2],schema_shift]
+                self.df.loc[idx[0],schema_shift] = 0 # Zeroing all the schema 
+        # Mirroring to get adversary teams
         for matchId in self.df['matchId'].unique():
             idx = self.df[self.df['matchId'] == matchId].index
             idxT = [idx[-1+ii] for ii in range(len(idx))]
-            self.df.loc[idx,'away_points'] = self.df.loc[idxT,'home_points']
-            self.df.loc[idx,'scored_goals_season_a'] = self.df.loc[idxT,'scored_goals_season_h']
-            self.df.loc[idx, 'missed_goals_season_a'] = self.df.loc[idxT,'missed_goals_season_h']
-            self.df.loc[idx, 'avg_ppda.att_a'] = self.df.loc[idxT,'avg_ppda.att_h']
-            self.df.loc[idx, 'avg_ppda.def_a'] = self.df.loc[idxT,'avg_ppda.def_h']
-            self.df.loc[idx, 'avg_ppda_allowed.att_a'] = self.df.loc[idxT,'avg_ppda_allowed.att_h']
-            self.df.loc[idx, 'avg_ppda_allowed.def_a'] = self.df.loc[idxT,'avg_ppda_allowed.def_h']
-            self.df.loc[idx, 'avg_deep_a'] = self.df.loc[idxT,'avg_deep_h']
-            self.df.loc[idx, 'avg_deep_allowed_a'] = self.df.loc[idxT,'avg_deep_allowed_h']
-        
+            self.df.loc[idx,'season_points_adv'] = self.df.loc[idxT,'season_points']
+            self.df.loc[idx,'scored_goals_season_adv'] = self.df.loc[idxT,'scored_goals_season']
+            self.df.loc[idx, 'missed_goals_season_adv'] = self.df.loc[idxT,'missed_goals_season']
+            self.df.loc[idx, 'avg_ppda.att_adv'] = self.df.loc[idxT,'avg_ppda.att']
+            self.df.loc[idx, 'avg_ppda.def_adv'] = self.df.loc[idxT,'avg_ppda.def']
+            self.df.loc[idx, 'avg_ppda_allowed.att_adv'] = self.df.loc[idxT,'avg_ppda_allowed.att']
+            self.df.loc[idx, 'avg_ppda_allowed.def_adv'] = self.df.loc[idxT,'avg_ppda_allowed.def']
+            self.df.loc[idx, 'avg_deep_adv'] = self.df.loc[idxT,'avg_deep']
+            self.df.loc[idx, 'avg_deep_allowed_adv'] = self.df.loc[idxT,'avg_deep_allowed']
+            """
+                        for ii in idx:
+                            
+                            if self.df.loc[ii,'h_a'] == 'a':
+                                self.df.loc[idx,'season_points_a'] = self.df.loc[ii,'season_points']
+                                self.df.loc[idx,'scored_goals_season_a'] = self.df.loc[ii,'scored_goals_season']
+                                self.df.loc[idx, 'missed_goals_season_a'] = self.df.loc[ii,'missed_goals_season']
+                                self.df.loc[idx, 'avg_ppda.att_a'] = self.df.loc[ii,'avg_ppda.att']
+                                self.df.loc[idx, 'avg_ppda.def_a'] = self.df.loc[ii,'avg_ppda.def']
+                                self.df.loc[idx, 'avg_ppda_allowed.att_a'] = self.df.loc[ii,'avg_ppda_allowed.att']
+                                self.df.loc[idx, 'avg_ppda_allowed.def_a'] = self.df.loc[ii,'avg_ppda_allowed.def']
+                                self.df.loc[idx, 'avg_deep_a'] = self.df.loc[ii,'avg_deep']
+                                self.df.loc[idx, 'avg_deep_allowed_a'] = self.df.loc[ii,'avg_deep_allowed']
+                            else:
+                                self.df.loc[idx, 'season_points_h'] = self.df.loc[ii,'season_points']
+                                self.df.loc[idx, 'scored_goals_season_h'] = self.df.loc[ii,'scored_goals_season']
+                                self.df.loc[idx, 'missed_goals_season_h'] = self.df.loc[ii,'missed_goals_season']
+                                self.df.loc[idx, 'avg_ppda.att_h'] = self.df.loc[ii,'avg_ppda.att']
+                                self.df.loc[idx, 'avg_ppda.def_h'] = self.df.loc[ii,'avg_ppda.def']
+                                self.df.loc[idx, 'avg_ppda_allowed.att_h'] = self.df.loc[ii,'avg_ppda_allowed.att']
+                                self.df.loc[idx, 'avg_ppda_allowed.def_h'] = self.df.loc[ii,'avg_ppda_allowed.def']
+                                self.df.loc[idx, 'avg_deep_h'] = self.df.loc[ii,'avg_deep']
+                                self.df.loc[idx, 'avg_deep_allowed_h'] = self.df.loc[ii,'avg_deep_allowed']
+            """     
     def __saveData(self):
         self.df.to_csv(self.output_path + 'dataset.csv')
 
